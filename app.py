93e5f8b9b15b13db20fb7765f5eb13facd7381e5
Revisisland9 <drewtjones007@gmail.com>
2025-06-18 20:25:47 -0500

    app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if not manual_mode:
                 page = doc[page_num]
                 if shipper_name:
                     text_to_insert = f"Shipper: {shipper_name}    Date: {today_str}"
-                    page.insert_text((110, 575), text_to_insert, fontsize=10, fontname="helv", fill=(0, 0, 0))
+                    page.insert_text((110, 715), text_to_insert, fontsize=11, fontname="helvb", fill=(0, 0, 0))
             combined_bol.insert_pdf(doc)
 
             for page in doc:
@@ -157,4 +157,3 @@ if not manual_mode:
             )
         else:
             st.warning("⚠️ No valid BOLs found in the uploaded file(s).")
-
